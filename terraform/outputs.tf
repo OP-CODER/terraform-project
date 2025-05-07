438c1461adcdb820fa40d24d07004ed1231cacf4
Mohammed Anas Omairuddin <121341548+OP-CODER@users.noreply.github.com>
2025-05-07 17:21:53 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "inventory" {
   value = <<EOT
 [frontend]
-c9.local ansible_host=${aws_instance.c8.public_ip}
+c8.local ansible_host=${aws_instance.c8.public_ip}
 [backend]
 u21.local ansible_host=${aws_instance.u21.public_ip}
