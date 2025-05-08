fe14f10e7dd32996af3d3d2f0e5306e826589666
Mohammed Anas Omairuddin <121341548+OP-CODER@users.noreply.github.com>
2025-05-08 15:20:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_instance" "c8" {
+resource "aws_instance" "frontend" {
   ami           = "ami-085386e29e44dacd7"
   instance_type = "t2.micro"
-  key_name      = var.public_key
-
+  key_name      = "jenkins-tf"
   tags = {
     Name = "c8.local"
   }
-
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > ../ansible/frontend_ip.txt"
   }
 }
-
-resource "aws_instance" "u21" {
-  ami           = "ami-084568db4383264d4"
+resource "aws_instance" "backend" {
+  ami           = "ami-0f9de6e2d2f067fca"
   instance_type = "t2.micro"
-  key_name      = var.public_key
-
+  key_name      = "jenkins-tf"
   tags = {
     Name = "u21.local"
   }
-
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > ../ansible/backend_ip.txt"
   }
-}
-# Output IP addresses for Ansible inventory
-output "c8_ip" {
-  value = aws_instance.c8.public_ip
-}
-output "u21_ip" {
-  value = aws_instance.u21.public_ip
 }
