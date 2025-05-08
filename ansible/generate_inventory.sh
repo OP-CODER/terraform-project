814d659c0aa25f1fd2698fab8138d60b040bd42f
Mohammed Anas Omairuddin <121341548+OP-CODER@users.noreply.github.com>
2025-05-08 17:42:36 +0530

    Update generate_inventory.sh

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
-
-FRONTEND_IP=$(terraform output -raw frontend_ip)
-BACKEND_IP=$(terraform output -raw backend_ip)
-
-cat <<EOF > inventory.ini
 [frontend]
-c8 ansible_host=${FRONTEND_IP} ansible_user=ec2-user
+frontend ansible_host=44.202.116.15 ansible_user=ec2-user
 
 [backend]
-u21 ansible_host=${BACKEND_IP} ansible_user=ubuntu
-EOF
+backend ansible_host=18.205.24.226 ansible_user=ubuntu
