c5ab8793c503a6964a848055ac5e162e7758ddc2
Mohammed Anas Omairuddin <121341548+OP-CODER@users.noreply.github.com>
2025-05-08 17:09:35 +0530

    Update generate_inventory.sh

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -1,5 +1,5 @@
 [frontend]
-c8 ansible_host=3.80.1.2 ansible_user=ec2-user
+c8 ansible_host=44.202.116.15 ansible_user=ec2-user
 
 [backend]
-u21 ansible_host=3.90.2.3 ansible_user=ubuntu
+u21 ansible_host=18.205.24.226 ansible_user=ubuntu
